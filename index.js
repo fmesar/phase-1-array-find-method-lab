20dfa14374fcac18be12ea39783e2f21c395efe2
Francisco Mesar <fmesar87@gmail.com>
2022-03-30 19:40:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+
+    const value = record.find(element => element.result === "W")
+    if(value === undefined) {
+        return undefined
+    } 
+
+    return value.year
+}
